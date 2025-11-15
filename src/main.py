f8b73974940eed73d0b434911978bc168b695fa5
xaviQuevedo <xavi_quevedo@hotmail.com>
2025-11-15 11:37:35 +0100

    add create

--- src/main.py
+++ src/main.py
@@ -5,4 +5,7 @@ NUMBER_OF_COLUMNS: Final[int] = 5
 
 
 def create_grid() -> list[list[int]]:
-    return None
+    listCol = [0 for x in range(NUMBER_OF_COLUMNS)]
+    listRow = [listCol for y in range(NUMBER_OF_ROWS)]        
+
+    return listRow
